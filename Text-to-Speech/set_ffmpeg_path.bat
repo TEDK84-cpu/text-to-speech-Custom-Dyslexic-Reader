e826f515675de45ffbbfe6ce47d2cd4925e6e53b
TEDK84-cpu <terryenln84@gmail.com>
2025-05-23 21:44:50 +0100

    Add files via upload

---
+++ Text-to-Speech/set_ffmpeg_path.bat
@@ -0,0 +1,4 @@
+@echo off 
+set "PATH=%PATH%;C:\Users\ted\Desktop\01. Cursor\0005\Text-to-Speech\\ffmpeg" 
+set "FFMPEG_PATH=C:\Users\ted\Desktop\01. Cursor\0005\Text-to-Speech\\ffmpeg" 
+start "" "run.bat" 
